6cad4c1178060ca5842e60b4131fdc4d2580adcb
Rishab K S <58295204+Rishab2707@users.noreply.github.com>
2021-12-05 16:31:55 +0530

    stream dai seko

--- main.py
+++ main.py
@@ -23,12 +23,25 @@ tweet_df1= tweet_df.selectExpr("CAST(value AS STRING)")
 
 tweetdf2=tweet_df1.withColumn('json',from_json(col('value'),json_schema)).select(explode(col('json')))
 tweetdf3=tweetdf2.withColumn('value',from_json(col('value'),json_schema)).withColumn('feature0',col('value.feature0')).withColumn('feature1',col('value.feature1')).drop('value')
-query = tweetdf3\
-    .writeStream \
-    .format("console") \
-    .option('truncate',False)\
-    .start()\
+
+def pre_process(tdf):
+    tdf = tdf.na.replace('', None)
+    tdf = tdf.na.drop()       
+    tdf = tdf.withColumn('feature1', regexp_replace('feature1',r'@\w+',''))
+    tdf = tdf.withColumn('feature1', regexp_replace('feature1','[^A-Za-z0-9\s]',''))
+    tdf = tdf.withColumn('feature1', regexp_replace('feature1',r'http\S+',''))
+    return tdf
+
+df = pre_process(tweetdf3)
+
+
+def p(b,id):
+    tokenizer = Tokenizer(inputCol="feature1", outputCol="words")
+    pipeline = Pipeline(stages=[tokenizer])
     
-
+    pipelineFit = pipeline.fit(b)
+    ddf = pipelineFit.transform(b)
+    ddf.show()
+    
+query = df.writeStream.foreachBatch(p).start()
 query.awaitTermination()
-
